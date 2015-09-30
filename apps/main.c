b493eea20597745eedd204744fb3604a3c0bcf88
hana <hana5638@gmail.com>
2015-09-29 23:35:08 -0700

    bug456 test

--- apps/main.c
+++ apps/main.c
@@ -3,11 +3,12 @@
 
 int main()
 {
+	printf("start test branch\n");
 	foo1();
 	foo2();
-	printf("it is changed.");
+	printf("it is second changed.");
 	printf("Test new branch");
-	printf("Bug123\n");
+	printf("Bug456\n");
 	pritnf("Second add\n");
 	printf("Update");
 }
